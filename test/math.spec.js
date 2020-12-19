4715c86a844aa1c584e2692029fea10fde76542a
WillianFarias <williansousafarias@gmail.com>
2020-12-18 22:06:11 -0300

    trabalhando com o rastrei da execucao de funcoes com sinon

--- test/math.spec.js
+++ test/math.spec.js
@@ -2,6 +2,7 @@
 const assert = require('assert');
 const Math = require('../src/math.js');
 const expect = require('chai').expect;
+const sinon = require('sinon');
 
 //valor dinamico
 let value = 0;
@@ -39,7 +40,7 @@ describe('Math class', function () {
   //possivel criar testes sobre funcoes que ainda nao existem
   //only define que apenas o teste especifico deve ser executado
   //skip iginora o teste
-  it.only('Multiply two numbers', function() {
+  it('Multiply two numbers', function() {
     const math = new Math();
     
     //validando obj
@@ -57,5 +58,33 @@ describe('Math class', function () {
     //expect(obj).to.have.property('name').equal('Celso');
     //expect(math.multiply(value, 5)).to.equal(0);
     //assert.equal(math.multiply(value, 5), 0);
+  });
+
+  it.only('Calls res with sum and index values', function() {
+    const req = {};
+    const res = {
+      //load: sinon.spy()
+      load: function load() {
+        console.log('Called!');
+      }
+    };
+
+    //utilizando spy em funcao já construida
+    //sinon.spy(res, 'load');
+    //apartir disto é possivel capturar libs e altera seus metodos
+    sinon.stub(res, 'load').returns('xpto');
+
+    const math = new Math();
+
+    math.printSum(req, res, 5, 5);
+
+    //cria uma funcao "espeiao" que observa se o metodo foi chamado ou nao
+    //expect(res.load.calledOnce).to.be.true;
+    //verificando o valor retornado
+    //retorna o metodo normal
+    res.restore();
+
+    expect(res.load.args[0][0]).to.equal('index');
+
   });
 });
